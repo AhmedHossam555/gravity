0bbc16272dfa2b68ae311d0a7e9fdcfda066324d
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-31 09:57:42 +0200

    dropdown responsive

--- index.js
+++ index.js
@@ -8,3 +8,36 @@ window.addEventListener('resize',(event)=>{
     ul.style.display = 'flex';
   }
 })
+
+// animation scroll in page
+
+const observe = new IntersectionObserver((entries)=>{
+  entries.forEach((ele)=>{
+    if(ele.isIntersecting){
+      ele.target.classList.add('show')
+    }else{
+      ele.target.classList.remove('show')
+    }
+  })
+})
+document.querySelectorAll('.hidden').forEach((ele)=> observe.observe(ele));
+
+
+const observes = new IntersectionObserver((entries)=>{
+  entries.forEach((ele)=>{
+    if(ele.isIntersecting){
+      ele.target.classList.add('show-para')
+    }else{
+      ele.target.classList.remove('show-para')
+    }
+  })
+})
+document.querySelectorAll('.hidden-para').forEach((ele)=> observes.observe(ele));
+
+
+///////////////
+const toggle = document.querySelector(".header .toggle");
+const dropdown = document.querySelector(".header .dropdown")
+toggle.addEventListener('click', ()=>{
+  dropdown.classList.toggle('active')
+})
